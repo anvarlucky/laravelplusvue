b97f167c149858077f38f6e4cdbf5a74c5988f7e
anvarlucky <anvarmath@gmail.com>
2022-10-03 12:12:36 +0500

    first

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!Doctype html>
+<html>
+<head>
+    <title>Test Vue</title>
+</head>
+<body>
+<div id="app">
+    <example-component></example-component>
+</div>
+    <script src="./js/app.js"></script>
+</body>
+</html>
